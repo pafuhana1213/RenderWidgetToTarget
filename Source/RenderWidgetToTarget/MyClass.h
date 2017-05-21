ec73353dc7ec0e253fd34de22d66673a96425e2a
pafuhana1213 <pafuhana@hotmail.co.jp>
2017-05-21 16:55:49 +0900

    サンプルプロジェクト・プラグイン作成

---
+++ Source/RenderWidgetToTarget/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class RENDERWIDGETTOTARGET_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
---
+++ Source/RenderWidgetToTarget/RenderWidgetToTarget.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RenderWidgetToTarget.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RenderWidgetToTarget, "RenderWidgetToTarget" );
+
